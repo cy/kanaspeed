406688aa34d4d6ee86473d7a5774e2c08382e300
Christine Yu <christine.tc.yu@gmail.com>
2016-01-13 20:06:26 -0500

    remove back

--- index.android.js
+++ index.android.js
@@ -40,38 +40,9 @@ class kanaspeed extends React.Component {
       <Navigator style={{flex:1}}
                   initialRoute={{id: 'MainMenu', name: 'MainMenu', index: 0}}
                   renderScene={this.renderScene}
-                  navigationBar={ <Navigator.NavigationBar routeMapper={NavigationBarRouteMapper} /> } 
        />
     );
   }
 }
-
-
-var NavigationBarRouteMapper = {
-  LeftButton(route, navigator, index, navState) {
-    if(index > 0) {
-      return (
-      <TouchableHighlight  style={{marginTop: 10}} onPress={() => {
-            if (index > 0) {
-              navigator.pop();
-            }
-        }}>
-       <Text>Back</Text>
-     </TouchableHighlight>
- )} else {
- return null}
- },
-  RightButton(route, navigator, index, navState) {
-    return null;
-  },
-  Title(route, navigator, index, navState) {
-    return null
-  }
-};
-
-
-var styles = StyleSheet.create({
-
-});
 
 AppRegistry.registerComponent('kanaspeed', () => kanaspeed);
